b5227f19cdf85b580c5b6cbb2dc3eaf64e13142e
Vetcher <vetcher@ngs.ru>
2016-10-15 21:26:16 +0300

    add comments

--- interface.py
+++ interface.py
@@ -2,6 +2,7 @@
 num_of_rangs = 50
 
 
+# возвращает строку с указанным кол-вом решеток(#)
 def high_of_column(n):
     s = ''
     for i in range(n):
@@ -9,12 +10,14 @@ def high_of_column(n):
     return s
 
 
+# возвращает строку - гистограмму на основании словаря. кол-во решеток в гистограмме не более num_of_rangs
 def histogram_of_symbols(dic):
     ret_string = ''
     l = sum([val for key, val in dic.items()])
     for key, value in dic.items():
-        percents_of_syms = value / l
-        num_of_cols = round(percents_of_syms * num_of_rangs)
-        ret_string += '{symbol} : {column}\n'.format(symbol=key,
-                                                     column=high_of_column(num_of_cols))
+        percents_of_syms = value / l  # частота появления символа
+        num_of_cols = round(percents_of_syms * num_of_rangs)  # "высота" колонки
+        ret_string += '{symbol}({per}%) : {column}\n'.format(symbol=key,
+                                                             column=high_of_column(num_of_cols),
+                                                             per=percents_of_syms)
     return ret_string
