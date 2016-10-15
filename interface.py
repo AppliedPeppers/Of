b5c1ae778c2fe1963ffa0075f68de98e88d65306
Vetcher <vetcher@ngs.ru>
2016-10-15 20:42:38 +0300

    cmt

--- interface.py
+++ interface.py
@@ -1,6 +1,16 @@
 
+num_of_rangs = 50
 
-def histogram_of_symbols(array):
+def high_of_column(n):
+    s = ''
+    for i in range(n):
+        
+    return s
+
+def histogram_of_symbols(dic):
     ret_string = ''
-    
+    l = len(dic)
+    percents_of_syms = [val / l for key, val in dic.items()]
+    for key, value in dic.items():
+        ret_string += high_of_column(n)
     return ret_string
