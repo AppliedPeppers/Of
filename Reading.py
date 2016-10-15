08acc887b9ca078175a83284fd41636695cd4d82
Evgeny <plat96@yandex.ru>
2016-10-15 21:55:38 +0300

    Done

--- Reading.py
+++ Reading.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import urllib.request
 def reading(url):
-    raw_data = urllib.request.urlopen("https://www.youtube.com/feed/subscriptions")
+    text=''
+    raw_data = urllib.request.urlopen(url)
     data=raw_data.read().decode('utf-8')
     data=BeautifulSoup(data, 'html.parser')
-    print(data.text)
-reading()
+    data=data.find_all(['b', 'p', 'a', 'body', 'br', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'i'])
+    for name in data:
+        text=text+name.get_text()
+    return text
