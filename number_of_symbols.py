4fa6317cd42035bdd715ad4eeabc639ea949fb26
TonyKat <anton.kalaturskii@yandex.ru>
2016-10-15 21:41:10 +0300

    1

--- number_of_symbols.py
+++ number_of_symbols.py
@@ -1,35 +1,17 @@
 
 
-"""def numbers_of_symbols(str):
-
-    dictionary = {'syms': 0 ,'number': 0}
+def numbers_of_symbols(str):
+    dictionary = {}
     i = 0
-    while(1):
-        if (str[i])
+    while (i < len(str)):
+        if (dictionary.get(str[i])) == None:
+            dictionary.setdefault(str[i], 1)
+        else:
+            other = [str[i], dictionary.get(str[i]) + 1]
+            dictionary.update([other])
+        i = i + 1
 
+    return dictionary
 
-    return number"""
-
-d = {}
-
-str = 'abcaaaa'
-i = 0
-num = 0
-other = ['a', 1]
-#print(len(str))
-
-while (i < len(str)):
-    if (str[i] == 'a'):
-        num = num + 1
-        other[1] = num
-    i = i + 1
-print(d.keys())
-d.update([other])
-print(d.keys())
-print(d.get('a'))\
-
-d.setdefault(str[1])
-print(d.keys())
-
-
-
+str = 'aaabbcc333/;'
+print(numbers_of_symbols(str))
