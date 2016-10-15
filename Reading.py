605be624ea49f7417d6796a323bffc52ce6348ac
Evgeny <plat96@yandex.ru>
2016-10-15 20:29:44 +0300

    Fix

--- Reading.py
+++ Reading.py
@@ -1,6 +1,6 @@
+from bs4 import BeautifulSoup
+import urllib.request
 def reading():
-    from bs4 import BeautifulSoup
-    import urllib.request
     raw_data = urllib.request.urlopen("https://www.youtube.com/feed/subscriptions")
     data=raw_data.read().decode('cp1251')
     print(data)
