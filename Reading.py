42c5138183eb66a843713111c8a8465a2cf8e779
Vetcher <vetcher@ngs.ru>
2016-10-15 22:08:42 +0300

    fixes

--- Reading.py
+++ Reading.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib.request
+
+
 def reading(url):
-    text=''
+    text = ''
     raw_data = urllib.request.urlopen(url)
-    data=raw_data.read().decode('utf-8')
-    data=BeautifulSoup(data, 'html.parser')
-    data=data.find_all(['b', 'p', 'a', 'body', 'br', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'i'])
+    data = raw_data.read().decode('utf-8')
+    data = BeautifulSoup(data, 'html.parser')
+    data = data.find_all(['b', 'p', 'a', 'body', 'br', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'i'])
     for name in data:
-        text=text+name.get_text()
+        text = text + name.get_text()
     return text
